4ef361b969185911cc7f8f9267f273f5f38fa9d2
mr-fool <mr-fool@users.noreply.github.com>
2019-11-27 16:57:23 -0700

    formating

---
+++ index.js
@@ -0,0 +1,14 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=calgary,canada&APPID=769ad0392e215e9ef6e05f7e41a22700', true);
+
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+}
+
+// Send request
+request.send();
