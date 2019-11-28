74b5c27444f4cab75bec59b1e436314572d1ea42
mr-fool <mr-fool@users.noreply.github.com>
2019-11-27 23:40:29 -0700

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+//const axios = require('axios');
 
 // Open a new connection, using the GET request on the URL endpoint
 axios.get( 'http://api.openweathermap.org/data/2.5/forecast?id=5913490&units=metric&cnt=5&APPID=769ad0392e215e9ef6e05f7e41a22700')
