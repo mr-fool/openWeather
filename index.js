6ed28777fcbd98bfd66b2e688a99fa4d1d5a4d55
mr-fool <mr-fool@users.noreply.github.com>
2019-11-27 19:21:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
 // Open a new connection, using the GET request on the URL endpoint
-axios.get( 'http://api.openweathermap.org/data/2.5/weather?q=calgary,canada&APPID=769ad0392e215e9ef6e05f7e41a22700')
+axios.get( 'http://api.openweathermap.org/data/2.5/weather?q=calgary,canada&units=metric&APPID=769ad0392e215e9ef6e05f7e41a22700')
 .then((response) => {
-  console.log(response.main.temp);
+  console.log(response);
 
 });
 
