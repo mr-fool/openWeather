dc31b8d58b8cdc50eeb2c60ae830f0550326aa43
mr-fool <mr-fool@users.noreply.github.com>
2019-11-27 19:57:53 -0700

    compose string

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 // Open a new connection, using the GET request on the URL endpoint
 axios.get( 'http://api.openweathermap.org/data/2.5/forecast?id=5913490&units=metric&cnt=5&APPID=769ad0392e215e9ef6e05f7e41a22700')
 .then((response) => {
-  console.log(response);
+  for (var i = 0; i < 5; i ++) {
+    console.log(response.list. + i +"main.temp");
+  }
   //window.document.write(response.data.main.temp);
 });
 
