093cc67be6b5447acec2638c5b39e5845f5c68d5
mr-fool <mr-fool@users.noreply.github.com>
2019-11-27 19:29:22 -0700

    write to html

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const axios = require('axios');
 axios.get( 'http://api.openweathermap.org/data/2.5/weather?q=calgary,canada&units=metric&APPID=769ad0392e215e9ef6e05f7e41a22700')
 .then((response) => {
   console.log(response.data.main.temp);
-
+  document.write(response.data.main.temp);
 });
 
