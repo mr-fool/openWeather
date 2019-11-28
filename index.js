3425d900979f0e99c6ebbe380038efed9d730c87
mr-fool <mr-fool@users.noreply.github.com>
2019-11-27 20:00:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 // Open a new connection, using the GET request on the URL endpoint
 axios.get( 'http://api.openweathermap.org/data/2.5/forecast?id=5913490&units=metric&cnt=5&APPID=769ad0392e215e9ef6e05f7e41a22700')
 .then((response) => {
-  for (var i = 0; i < 5; i ++) {
+  var i;
+  for ( i = 0; i < 5; i ++) {
     console.log(response.list. + i +"main.temp");
   }
   //window.document.write(response.data.main.temp);
