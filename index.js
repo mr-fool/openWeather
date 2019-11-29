57d1e8e2883b653c8cf598b9f01c0cb9ad409e82
mr-fool <mr-fool@users.noreply.github.com>
2019-11-28 20:11:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function loaded() {
               event.preventDefault();
               var city = document.getElementById('city');
               var countryCode = document.getElementById('countryCode');
-              var day = moment().format("MMM Do YY");
               // Open a new connection, using the GET request on the URL endpoint
               axios.get( 'http://api.openweathermap.org/data/2.5/forecast?q='+ city.value + "," + countryCode.value + '&units=metric&cnt=5&APPID=769ad0392e215e9ef6e05f7e41a22700')
               .then((response) => {
@@ -13,8 +12,10 @@ function loaded() {
                   var tomorrow = new Date();
                   //tomorrow.setDate(tomorrow.getDate() + i); //can't figure out how to set local time instead to format 2019-month-day
                   //console.log(tomorrow); 
+                  var day = i + 1;
+                  document.write("Day " + day +"<br>");
                   console.log(response.data.list[i].main.temp);
-                  document.write("Temperature " + response.data.list[i].main.temp + "<br>");
+                  document.write("Temperature " + response.data.list[i].main.temp + "C" + "<br>");
                   console.log(response.data.list[i].weather[0].main);
                   document.write("Precipitation " + response.data.list[i].weather[0].main + "<br><br>");
                 }
